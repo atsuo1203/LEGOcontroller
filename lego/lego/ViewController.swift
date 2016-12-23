f06d1529ac5f691f403d6da9a7aa9b34188c0a35
atsuo1203 <atsuo@cps.im.dendai.ac.jp>
2016-12-23 11:15:32 +0900

    buttonをつけた

---
+++ lego/lego/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  lego
+//
+//  Created by Atsuo Yonehara on 2016/12/23.
+//  Copyright © 2016年 Atsuo Yonehara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBAction func upButton(_ sender: UIButton) {
+    }
+    @IBAction func downButton(_ sender: UIButton) {
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
