ecdcb7d668afd5e9218bddfeee806b194d34a6a7
atsuo1203 <atsuo@cps.im.dendai.ac.jp>
2017-01-13 15:51:10 +0900

    エラー解除

--- lego/lego/ViewController.swift
+++ lego/lego/ViewController.swift
@@ -11,19 +11,12 @@ import UIKit
 class ViewController: UIViewController {
     @IBAction func upButton(_ sender: UIButton) {
     }
+    
     @IBAction func downButton(_ sender: UIButton) {
     }
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
-
